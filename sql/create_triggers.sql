c4777467e7722e2a0b44ecdca795338d4b301817
Lexi Pierre <elexiapierre@gmail.com>
2018-03-11 19:49:00 -0700

    Added contract checking trigger for repairjobs
    Checks if the contract can be covered. (That time of arrival is within the start/end dates)

--- sql/create_triggers.sql
+++ sql/create_triggers.sql
@@ -1,11 +1,12 @@
 -- Create a trigger to remove a RepairJob when it's status is 'DONE', and insert into
 -- a new table "RepairLog"
 
+-- Move the information into RepairLog
 CREATE OR REPLACE TRIGGER job_done_log_trig
     AFTER  UPDATE ON RepairJobs
     FOR EACH ROW
 	BEGIN
-		--If the status is DONE, move data to Repair Log and DELETE
+		--If the status is DONE, move data to Repair Log
 		IF (:new.status = 'DONE') THEN
 			INSERT INTO RepairLog VALUES			
 				(:new.repairId, :new.employeeNo, :new.phone, :new.machineID, 
@@ -15,11 +16,39 @@ CREATE OR REPLACE TRIGGER job_done_log_trig
 /
 Show errors;
 
+-- Delete the row
 CREATE OR REPLACE TRIGGER job_done_delete_trig
     AFTER  UPDATE ON RepairJobs
 	BEGIN
+		--If the status is DONE, delete the row
 		DELETE FROM RepairJobs WHERE status = 'DONE';
     END;
 /
 Show errors;
 
+-- Create a trigger to verify that serviceIds inserted into RepairJobs are valid
+-- (Between the start and end date)
+CREATE OR REPLACE TRIGGER check_contract_trig
+	BEFORE INSERT ON RepairJobs
+	FOR EACH ROW
+	DECLARE
+		sid VARCHAR(20);
+		tin TIMESTAMP;
+		contractStart DATE; 
+		contractEnd DATE;
+	BEGIN
+
+		sid := :new.serviceContractId;
+		tin := :new.timeOfArrival;
+		
+		SELECT startDate INTO contractStart FROM ServiceContracts WHERE contractId = sid;
+		SELECT endDate INTO contractEnd FROM ServiceContracts WHERE contractId = sid;
+
+		--If the date isn't within the range, error
+		IF(NOT(tin BETWEEN contractStart and contractEnd)) THEN
+			RAISE_APPLICATION_ERROR(-20101, 'The given contract cannot be used at this time. (Expired or not active yet.)');
+    		ROLLBACK;
+		END IF;
+	END;
+/
+Show errors;
